dba2ef098d049c5cf495bd8ce97d52607c6eb40c
iihayan <70726326+iihayan@users.noreply.github.com>
2020-10-23 20:34:41 -0400

    completing the first tutorial

--- content.js
+++ content.js
@@ -2,5 +2,14 @@ var elements = document.getElementsByTagName('p');
 
 for (var i = 0; i < elements.length; i++) {
    var element = elements[i];
-}
+   //console.log ( element );
+   //element.style.setProperty('border','solid 1px red');
 
+   var r = Math.floor(Math.random()*255);
+   var g = Math.floor(Math.random()*100);
+   var b = Math.floor(Math.random()*100);
+   var c = "rgb(" + r + "," + g + "," + b + ")";
+
+   element.style.setProperty('background-color',c);
+
+}
